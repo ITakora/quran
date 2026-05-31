2d7c31dba72e3107712ef77d95241c6f61bf7f54
ITakora <oyamitakora@gmail.com>
2026-05-31 16:56:09 +0700

    fixing bug detail surah text latin by using utf8

--- lib/network/api/surah_detail_api.dart
+++ lib/network/api/surah_detail_api.dart
@@ -1,15 +1,16 @@
 import 'dart:convert';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:quran/network/models/detail_surah_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:quran/network/models/detail_surah_model.dart';
 
 class DetailSurahApi {
   Future<List<DetailSurah>> getDetailSurah({required number}) async {
     final Uri url = Uri.parse('https://equran.id/api/v2/surat/$number');
     final response = await http.get(url);
+    final String decodedBody = utf8.decode(response.bodyBytes);
     final List data =
-        (jsonDecode(response.body) as Map<String, dynamic>)['data']['ayat'];
+        (jsonDecode(decodedBody) as Map<String, dynamic>)['data']['ayat'];
 
     final List<DetailSurah> detailSurah =
         data.map((data) => DetailSurah.fromJson(data)).toList();
